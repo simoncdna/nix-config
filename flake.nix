9a68395ed1e208c88702981eb5e07a0197df5b0d
Simon Cardona <simon.cdna@proton.me>
2026-01-12 20:09:17 +0100

    feat: initialize flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+	description = "Simon's Nix system configuration";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+		home-manager = {
+			url = "github:nix-community/home-manager";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+	};
+
+	outputs = 
+		{ ... }@inputs: {
+			nixosConfigurations = {
+				nixos = inputs.nixpkgs.lib.nixosSystem {
+					system = "aarch64-linux";
+					specialArgs = { inherit inputs; };
+					modules = [
+						./hosts/nixos/configuration.nix
+
+						inputs.home-manager.nixosModules.home-manager
+
+						{
+							home-manager.useGlobalPkgs = true;
+							home-manager.useUserPackages = true;
+							home-manager.users.simon = import ./home/profiles/default.nix;
+							home-manager.extraSpecialArgs = { inherit inputs; };
+						}
+					];
+				};
+			};
+	};
+}
