c87e03f3c8be5dd046878eff6e08ba4ecb22290f
Simon Cardona <simon.cdna@proton.me>
2026-01-14 18:52:10 +0100

    fix(modules/zsh): add oh-my-zsh conf

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -6,11 +6,17 @@
 		autosuggestion.enable = true;
 		syntaxHighlighting.enable = true;
 
+		oh-my-zsh = {
+			enable = true;
+			theme = "robbyrussell";
+			plugins = [ "git" "sudo" "history" "fzf" "web-search" ];
+		};
+
 		shellAliases = {
 			vim = "nvim";
 		};
 
-		initExtra = ''
+		initContent = ''
 			export EDITOR=nvim
 			export PATH="$HOME/.local/bin:$PATH"
 			'';
