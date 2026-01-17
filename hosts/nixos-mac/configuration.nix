4f5c05114a119a4c37081867576d7803f0670587
Simon Cardona <simon.cdna@proton.me>
2026-01-17 22:19:32 +0100

    fix(hosts/nixos-mac): configuration

--- hosts/nixos-mac/configuration.nix
+++ hosts/nixos-mac/configuration.nix
@@ -14,8 +14,10 @@
 	# Enable notch
 	boot.kernelParams = [ "apple_dcp.show_notch=1" ];
 
-  # Disable automatic firmware extraction (it's already installed)
-  hardware.asahi.extractPeripheralFirmware = false;
+  # Enable automatic firmware extraction with local firmware directory
+  hardware.asahi.extractPeripheralFirmware = true;
+  hardware.asahi.peripheralFirmwareDirectory = /boot/asahi;
+	hardware.graphics.enable = true;
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -69,142 +71,7 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
 
-  # Default Shell
-  programs.zsh = {
-  	enable = true;
-
-	autosuggestions.enable = true;
-	syntaxHighlighting.enable = true;
-
-	ohMyZsh = {
-		enable = true;
-		theme = "robbyrussell";
-		plugins = [ "git" "sudo" "history" "fzf" "web-search" ];
-	};
-  };
-
-  programs.starship.enable = true;
-
-  # Tmux
-  programs.tmux = {
-    enable = true;
-    keyMode = "vi";
-    baseIndex = 1;
-    terminal = "screen-256color";
-    escapeTime = 0;
-    historyLimit = 10000;
-
-    plugins = with pkgs.tmuxPlugins; [
-	vim-tmux-navigator
-	sensible
-	sessionist
-	fzf-tmux-url
-	online-status
-	battery
-	resurrect
-	continuum
-	catppuccin
-    ];
-
-    extraConfig = ''
-        set-option -g prefix C-Space
-     
-        # Enable mouse
-        set -g mouse on
-        set -g default-shell $SHELL
-        set -ga terminal-overrides ",foot:Tc"
- 
- 	set-option -g status-position top #
-
-        set -g @resurrect-capture-pane-contents 'on'
-        set -g @continuum-restore 'on'
-        set -g @catppuccin_flavor "mocha"
-        set -g @catppuccin_status_background "none"
-        set -g @catppuccin_window_status_style "none"
-        set -g @catppuccin_pane_status_enabled "off"
-        set -g @catppuccin_pane_border_status "off"
-
-	# Pane resizing
-	bind -r C-k resize-pane -U
-	bind -r C-j resize-pane -D
-	bind -r C-h resize-pane -L
-
-	# Copy mode
-	bind-key v copy-mode
-	bind-key -T copy-mode-vi v send-keys -X begin-selection
-	bind-key -T copy-mode-vi y send-keys -X copy-selection
-	bind-key -T copy-mode-vi r send-keys -X rectangle-toggle
-	
-	# Go to previous window
-	bind -r O switch-client -l
-	bind -r o last-window
-	bind -r ^ switch-client -l
-	
-	# Go to previous window
-	bind -r N switch-client -n
-	bind -r P switch-client -p
-	
-	# Vim like pane splitting
-	bind H split-window -v
-	bind V split-window -h
-	
-	# Start windows and panes at 1 and not 0
-set -g base-index 1
-set -g pane-base-index 1
-set-window-option -g pane-base-index 1
-set-option -g renumber-windows on
-	
-	# Undercurl
-	set -g default-terminal "''${TERM}"
-	set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'  # undercurl support
-	set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'  # underscore colours - needs tmux-3.0
-	
-	# Toggle tmux status bar
-	bind '\' set status
-	if-shell "[[ $(tmux lsw | wc -l) -le 1 ]]" 'set -g status'
-
-	set -g status on
-
-	# Configure Online status
-	set -g @online_icon "ok"
-	set -g @offline_icon "nok"
-	
-	# status left look and feel
-	set -g status-left-length 100
-	set -g status-left ""
-	set -ga status-left "#{?client_prefix,#{#[bg=#{@thm_red},fg=#{@thm_bg},bold]  #S },#{#[bg=#{@thm_bg},fg=#{@thm_green}]  #S }}"
-	set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_overlay_0},none]│"
-	set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_blue}]  #{=/-32/...:#{s|$USER|~|:#{b:pane_current_path}}} "
-	set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_overlay_0},none]#{?window_zoomed_flag,│,}"
-	set -ga status-left "#[bg=#{@thm_bg},fg=#{@thm_yellow}]#{?window_zoomed_flag,  zoom ,}"
-	
-	# status right look and feel
-	set -g status-right-length 100
-	set -g status-right ""
-	set -ga status-right "#{?#{e|>=:10,#{battery_percentage}},#{#[bg=#{@thm_red},fg=#{@thm_bg}]},#{#[bg=#{@thm_bg},fg=#{@thm_pink}]}} #{battery_icon} #{battery_percentage} "
-	set -ga status-right "#[bg=#{@thm_bg},fg=#{@thm_overlay_0}, none]│"
-	set -ga status-right "#[bg=#{@thm_bg}]#{?#{==:#{online_status},ok},#[fg=#{@thm_mauve}] 󰖩 on ,#[fg=#{@thm_red},bold]#[reverse] 󰖪 off }"
-	
-	# Configure Tmux
-	set -g status-position bottom
-	set -g status-style "bg=#{@thm_bg}"
-	set -g status-justify "absolute-centre"
-
-	# window look and feel
-	set -wg automatic-rename on
-	set -g automatic-rename-format "#{pane_current_command}"
-	
-	set -g window-status-format " #I#{?#{!=:#{window_name},Window},: #W,} "
-	set -g window-status-style "bg=#{@thm_bg},fg=#{@thm_rosewater}"
-	set -g window-status-last-style "bg=#{@thm_bg},fg=#{@thm_peach}"
-	set -g window-status-activity-style "bg=#{@thm_red},fg=#{@thm_bg}"
-	set -g window-status-bell-style "bg=#{@thm_red},fg=#{@thm_bg},bold"
-	set -gF window-status-separator "#[bg=#{@thm_bg},fg=#{@thm_overlay_0}]│"
-	
-	set -g window-status-current-format " #I#{?#{!=:#{window_name},Window},: #W,} "
-	set -g window-status-current-style "bg=#{@thm_peach},fg=#{@thm_bg},bold"
-'';
-	};
+	programs.zsh.enable = true;
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.simon = {
     isNormalUser = true;
@@ -217,13 +84,6 @@ set-option -g renumber-windows on
   };
 
   # programs.firefox.enable = true;
-
-  # SwayFX configuration
-  programs.sway = {
-  	enable = true;
-	package = pkgs.swayfx;
-	wrapperFeatures.gtk = true;
-  };
 
   # Login manager minimal
   # services.greetd = {
@@ -252,22 +112,23 @@ set-option -g renumber-windows on
      waybar
      mako
      networkmanagerapplet
-     quickshell
-     qt6.qtwayland
      brightnessctl
 
      wl-clipboard
      grim
      swaylock
      swayidle
+		 fzf
 
      claude-code
+     devenv
 
      nodejs_22
      nodePackages.npm
      nodePackages.pnpm
 
      gcc
+		 gnumake
      clang
      llvm
      cargo
@@ -275,6 +136,7 @@ set-option -g renumber-windows on
 
   # Activate flakes
   nix.settings.experimental-features = [ "nix-command" "flakes"];
+  nix.settings.trusted-users = [ "root" "simon" ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
@@ -317,6 +179,6 @@ set-option -g renumber-windows on
   # and migrated your data accordingly.
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "25.11"; # Did you read the comment?
+  system.stateVersion = "25.11";
 
 }
