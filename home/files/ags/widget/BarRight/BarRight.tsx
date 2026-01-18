86243fe2869228e993b0607f305a2390d737b9d8
Simon Cardona <simon.cdna@proton.me>
2026-01-18 19:26:00 +0100

    feat(files/ags/widgets/wifi): improve widget

--- home/files/ags/widget/BarRight/BarRight.tsx
+++ home/files/ags/widget/BarRight/BarRight.tsx
@@ -1,13 +1,12 @@
 import { Gtk } from "ags/gtk4";
 import Battery from "../Battery/Battery";
 import { createPoll } from "ags/time";
+import Wifi from "../Wifi/Wifi";
+import PowerMenu from "../PowerMenu/PowerMenu";
 
 export default function BarRight() {
 	const time = createPoll("", 1000, () => {
     return new Date().toLocaleTimeString("en-US", {
-			weekday: "long",
-			day: "numeric",
-			month: "short",
       hour: "2-digit",
       minute: "2-digit",
       timeZone: "Europe/Paris"
@@ -16,13 +15,15 @@ export default function BarRight() {
 
 	return (
 		<box cssName="right" $type="end" hexpand halign={Gtk.Align.END}>
+			<Wifi />
 			<Battery />
-			<menubutton cssName="btn-2" $type="end" hexpand halign={Gtk.Align.END}>
+			<menubutton cssName="btn-2">
 				<label label={time} />
-				<popover>
+				<popover hasArrow={false}>
 					<Gtk.Calendar />
 				</popover>
 			</menubutton>
+			<PowerMenu />
 		</box>
 	);
 }
--- home/files/ags/widget/Wifi/Wifi.tsx
+++ home/files/ags/widget/Wifi/Wifi.tsx
@@ -1,35 +1,92 @@
+import { Gtk } from "ags/gtk4";
+import { createPoll } from "ags/time";
 import Network from "gi://AstalNetwork";
-import { Accessor, createBinding } from "gnim";
+import { Accessor, For, With } from "gnim";
 
 export default function Wifi() {
 	const network = Network.get_default();
 
-	const enabled = createBinding(network, "wifi", "enabled");
-	const state = createBinding(network, "wifi", "state");
+	const wifiState = createPoll({ enabled: false, state: 0 }, 500, () => ({
+		enabled: network.wifi.enabled,
+		state: network.wifi.state
+	}));
+
+	const accessPoints = createPoll([], 2000, () => network.wifi.accessPoints);
+
 
 	function getWifiLogo(enabled: boolean, state: number): string {
-		if (!enabled) return "󰖪"
-
 		if (state !== Network.DeviceState.ACTIVATED) return "󰤮"
+
+		if (!enabled) return "󰖪"
 
 		return ""
 	}
 
 	function getWifiClass(enabled: boolean, state: number): string {
-		if (!enabled) return "disabled"
-
 		if (state !== Network.DeviceState.ACTIVATED) return "disactivated"
+
+		if (!enabled) return "disabled"
 
 		return "activated"
 	}
 
-	return (
-		<box>
-			<label 
-				class={`wifi-logo ${enabled.as(e => getWifiClass(e, state.get()))}`} 
-				label={enabled.as(e => getWifiLogo(e, state.get()))}
-			/>
-		</box>
+	function isSecured(ap: Network.AccessPoint): boolean {
+		return ap.flags !== 0 || ap.wpaFlags !== 0 || ap.rsnFlags !== 0;
+	}
 
+	return (
+		<menubutton class="wifi">
+			<With value={wifiState}>
+				{(w) => (
+					<label
+						class={`wifi-logo ${getWifiClass(w.enabled, w.state)}`}
+						label={getWifiLogo(w.enabled, w.state)}
+					/>
+				)}
+			</With>
+			<popover class="popover" hasArrow={false}>
+				<box orientation={Gtk.Orientation.VERTICAL} vexpand>
+					<box class="toggle-wrapper" valign={Gtk.Align.START} vexpand hexpand>
+						<label label="Wifi" hexpand halign={Gtk.Align.START} />
+						<With value={wifiState}>
+							{(w) => (
+								<switch
+									class="switch"
+									hexpand
+									halign={Gtk.Align.END}
+									active={w.enabled}
+									onNotifyActive={(self) => {
+										network.wifi.enabled = self.active;
+									}}
+								/>
+							)}
+						</With>
+					</box>
+
+					<box class="list-wrapper" valign={Gtk.Align.END} hexpand orientation={Gtk.Orientation.VERTICAL}>
+						<For each={accessPoints}>
+							{(ap) => {
+								return (
+									<box class="network" hexpand>
+										{ap.ssid === network.wifi.activeAccessPoint?.ssid ? (
+											<label widthChars={2} xalign={0.5} class="icon active" label="󰄬" />
+										) : (
+											<label widthChars={2} xalign={0.5} class="icon" label=""
+											/>
+										)}
+										<box hexpand>
+											<label class="ssid" label={ap.ssid} />
+										</box>
+										{isSecured(ap) && (
+											<label widthChars={2} xalign={0.5} class="icon" label="󰌾" />
+										)}
+									</box>
+								)
+							}}
+						</For>
+					</box>
+				</box>
+			</popover>
+		</menubutton>
 	);
 }
