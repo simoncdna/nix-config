d84fda5f10f0ed1b68356e07159311bf87c6494a
Simon Cardona <simon.cdna@proton.me>
2026-01-18 11:13:26 +0100

    feat(files/ags/widgets/battery): change style

--- home/files/ags/widget/Battery/Battery.tsx
+++ home/files/ags/widget/Battery/Battery.tsx
@@ -1,22 +1,43 @@
+import { exec } from "ags/process";
 import { createPoll } from "ags/time";
-import { Accessor } from "gnim";
+import { Accessor, With } from "gnim";
 
 export default function Battery() {
-	const battery = createPoll("", 5000, "cat /sys/class/power_supply/macsmc-battery/capacity")
+	const battery = createPoll({ capacity: 0, isCharging: false }, 500, () => {
+		const capacity = parseInt(exec("cat /sys/class/power_supply/macsmc-battery/capacity"));
+		const isCharging = exec("cat /sys/class/power_supply/macsmc-battery/status") === "Charging";
 
-	 function getBatteryLogo(battery: string): string {
-      const level = parseInt(battery)
+		return { capacity, isCharging }
+	});
 
-      if (level > 80) return "󱊣"
-      if (level > 50) return "󱊢"
-			if (level > 20) return "󱊡"
-      return "󰂎"
-		}
+	function getBatteryLogo(level: number, isCharging: boolean): string {
+		if (isCharging) return "󰂄"
+		if (level > 80) return "󱊣"
+		if (level > 50) return "󱊢"
+		if (level > 20) return "󱊡"
+		return "󰂎"
+	}
+
+	function getBatteryClass(level: number, isCharging: boolean): string {
+		if (isCharging) return "charging"
+		if (level > 80) return "high"
+		if (level > 50) return "medium"
+		if (level > 20) return "low"
+		return "critical"
+	}
 
 	return (
-		<box class="battery">
-			<label class="battery-logo" label={battery.as(b => getBatteryLogo(b))} />
-			<label label={battery.as(b => `${b.trim()}%`)} />
+		<box>
+			<With value={battery}>
+				{(b) => {
+					return (
+						<box class="battery">
+							<label class={`battery-logo ${getBatteryClass(b.capacity, b.isCharging)}`} label={getBatteryLogo(b.capacity, b.isCharging)} />
+							<label label={`${b.capacity}%`} />
+						</box>
+					)
+				}}
+			</With>
 		</box>
 	);
 }
